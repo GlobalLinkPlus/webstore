9abad3b4925b7b4445fdc7df6e271d43bd46e84e
MiguelShinyenyi <miguel.shinyenyi@globallinkplus.com>
2023-10-18 15:08:06 +0300

    set favicon image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { BizService } from './services/biz.service';
+import { AngularFaviconService } from 'angular-favicon';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'ecommerce';
- constructor(public bizService: BizService){
+ constructor(public bizService: BizService,
+  private ngxFavicon: AngularFaviconService){
 
  }
-
+ ngOnInit() {
+  this.ngxFavicon.setFavicon(this.bizService.get_favicon_image());
+  // this.ngxFavicon.setFavicon("https://static.vecteezy.com/system/resources/thumbnails/003/171/355/small/objective-lens-icon-with-six-rainbow-colors-vector.jpg");
+}
 }
