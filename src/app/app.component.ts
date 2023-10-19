7214d34d3ff6a9aafb6c98db1cb9ceb3b9745157
MiguelShinyenyi <miguel.shinyenyi@globallinkplus.com>
2023-10-19 17:30:33 +0300

    set meta data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BizService } from './services/biz.service';
 import { AngularFaviconService } from 'angular-favicon';
+import { Title} from '@angular/platform-browser';
+import { Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,18 @@ import { AngularFaviconService } from 'angular-favicon';
 export class AppComponent implements OnInit{
   title = 'ecommerce';
  constructor(public bizService: BizService,
-  private ngxFavicon: AngularFaviconService){
+  private ngxFavicon: AngularFaviconService,
+  private titleService: Title,
+  private metaService: Meta
+  ){
 
  }
  ngOnInit() {
+  this.titleService.setTitle(this.bizService.get_meta_title());
+  this.metaService.addTags([
+    {name: 'keywords', content: 'your keywords content'},
+    {name: 'description', content: this.bizService.get_meta_description()},
+  ]);
   this.ngxFavicon.setFavicon(this.bizService.get_favicon_image());
   // this.ngxFavicon.setFavicon("https://static.vecteezy.com/system/resources/thumbnails/003/171/355/small/objective-lens-icon-with-six-rainbow-colors-vector.jpg");
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,16 +11,16 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  section_one: any={
-    name:"",
-    products:[]
+  section_one: any = {
+    name: "",
+    products: []
   };
-  section_two: any={
-    name:"",
-    products:[]
+  section_two: any = {
+    name: "",
+    products: []
   };
 
-  loading=false;
+  loading = false;
 
 
 
@@ -29,23 +29,23 @@ export class HomeComponent implements OnInit {
 
   partners: any[] = [
     {
-    "image":"assets/img/amazon.jpg",
-    "name":"Amazon"
-  },
-  {
-    "image":"assets/img/alibaba.png",
-    "name":"Alibaba"
-  },
-  {
-    "image":"assets/img/fedex.png",
-    "name":"Fedex"
-  },
-  {
-    "image":"assets/img/f6s.jpg",
-    "name":"F6s"
-  }
+      "image": "assets/img/amazon.jpg",
+      "name": "Amazon"
+    },
+    {
+      "image": "assets/img/alibaba.png",
+      "name": "Alibaba"
+    },
+    {
+      "image": "assets/img/fedex.png",
+      "name": "Fedex"
+    },
+    {
+      "image": "assets/img/f6s.jpg",
+      "name": "F6s"
+    }
 
-];
+  ];
   totalCards: number = this.partners.length;
   currentPage: number = 1;
   pagePosition: string = "0%";
@@ -87,13 +87,12 @@ export class HomeComponent implements OnInit {
     public userInfoService: UserInfoService,
     public bizService: BizService,
     config: NgbCarouselConfig
-    ) { 
-      config.interval = 2000;
-
+  ) {
+    config.interval = 2000;
     config.keyboard = true;
-
     config.pauseOnHover = true;
-    }
+    config.wrap=false;
+  }
 
   ngOnInit(): void {
     // this.cardsPerPage = this.getCardsPerPage();
@@ -103,40 +102,40 @@ export class HomeComponent implements OnInit {
     // this.getTopProducts();
 
     this.getHomeSectionItems();
-    this.sliders=this.bizService.get_sliders()
+    this.sliders = this.bizService.get_sliders()
 
     // this.sliders.forEach(item => {
     //   this.images.push(item.image);
     // });
   }
-  getHomeSectionItems(){
-    this.loading=true;
-    this.apiService.getHomeSectionItems(this.bizService.get_company_id()).subscribe(res=>{
-      this.loading=false;    
-      this.section_one=res.section_one;
-        this.section_two=res.section_two;
-        },err=>{
-          this.loading=false; 
-          console.log("[ERROR]>>>",err);
-        });
-  }
-  getTopProducts(){
-    this.loading=true;
-    this.apiService.getTopProducts().subscribe(res=>{
-      this.loading=false;   
-      this.section_one.products=res;
-      this.section_one.name="Top Products";
-      this.section_two.products=res;
-      this.section_two.name="Hot line";
-    },err=>{
-      this.loading=false; 
-      console.log("[ERROR]>>>",err);
+  getHomeSectionItems() {
+    this.loading = true;
+    this.apiService.getHomeSectionItems(this.bizService.get_company_id()).subscribe(res => {
+      this.loading = false;
+      this.section_one = res.section_one;
+      this.section_two = res.section_two;
+    }, err => {
+      this.loading = false;
+      console.log("[ERROR]>>>", err);
     });
-  
+  }
+  getTopProducts() {
+    this.loading = true;
+    this.apiService.getTopProducts().subscribe(res => {
+      this.loading = false;
+      this.section_one.products = res;
+      this.section_one.name = "Top Products";
+      this.section_two.products = res;
+      this.section_two.name = "Hot line";
+    }, err => {
+      this.loading = false;
+      console.log("[ERROR]>>>", err);
+    });
+
   }
 
-  formatImageUrlToCss(src){
-    return 'url('+src+')';
+  formatImageUrlToCss(src) {
+    return 'url(' + src + ')';
 
   }
 
