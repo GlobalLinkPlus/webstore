ce70c55a2686f846f1274b32d27504ae58b806fa
MiguelShinyenyi <miguel.shinyenyi@globallinkplus.com>
2024-05-28 15:28:27 +0300

    make google key an env variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  GOOGLE_API_KEY: 'AIzaSyAYSk6F_zStBqewtptxnDPMOm6V9CfR-Pk',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GOOGLE_API_KEY: 'AIzaSyAYSk6F_zStBqewtptxnDPMOm6V9CfR-Pk',
 };
 
 /*
